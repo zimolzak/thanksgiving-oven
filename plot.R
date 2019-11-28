8e8868a736500b3308ed9a660be4c94208607bd3
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-11-28 15:32:44 -0600

    plot

--- plot.R
+++ plot.R
@@ -1,2 +1,3 @@
 library(ggplot2)
 X = read.csv('~/Desktop/thanksgiving-oven/data.csv')
+ggplot(X, aes(xmin = 0, xmax = minutes, ymin = 0, ymax = degreesf, x=minutes, y=degreesf, label = name)) +  geom_rect(alpha = 0.4) + geom_text() 
